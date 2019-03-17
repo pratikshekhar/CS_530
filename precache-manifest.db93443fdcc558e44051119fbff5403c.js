8877bf7be5c4e8913e25bbc1e3cb0bd639b2e7dc
Chetan Parakh <chetanparakh2@gmail.com>
2019-03-17 19:09:36 -0400

    Updates

---
+++ precache-manifest.db93443fdcc558e44051119fbff5403c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "621c22aad05949852e03",
+    "url": "/CS_530/static/js/runtime~main.12e168ec.js"
+  },
+  {
+    "revision": "6138cab33502dba2e654",
+    "url": "/CS_530/static/js/main.28f991a3.chunk.js"
+  },
+  {
+    "revision": "9a00acac85b51025a92c",
+    "url": "/CS_530/static/js/2.a8532c66.chunk.js"
+  },
+  {
+    "revision": "6138cab33502dba2e654",
+    "url": "/CS_530/static/css/main.b01be610.chunk.css"
+  },
+  {
+    "revision": "9a00acac85b51025a92c",
+    "url": "/CS_530/static/css/2.24bdf607.chunk.css"
+  },
+  {
+    "revision": "17c9a953982b093654a2de305d71ca17",
+    "url": "/CS_530/index.html"
+  }
+];
